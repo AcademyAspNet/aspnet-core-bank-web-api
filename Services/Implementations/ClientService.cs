f057be1320afe5b1f2af529de90b0ec210af4634
AcademyAspNet <elprofesor.espana@gmail.com>
2025-08-20 20:31:52 +0300

    Partial changes

---
+++ Services/Implementations/ClientService.cs
@@ -0,0 +1,36 @@
+﻿using _BankWebAPI.Data;
+using _BankWebAPI.Data.Entities;
+using _BankWebAPI.Models.DTOs;
+
+namespace _BankWebAPI.Services.Implementations
+{
+    public class ClientService : IClientService
+    {
+        private readonly ApplicationDbContext _database;
+
+        public ClientService(ApplicationDbContext database)
+        {
+            _database = database;
+        }
+
+        public IList<Client> GetClients(long fromClientId, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client? GetClientById(long clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client CreateClient(ClientDto clientDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteClientById(long clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
